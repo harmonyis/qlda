bc7e9e3a20adacf54ae4d2a3e5db64b2a8abab5c
Hoang The Anh <theanhkazu@hoangtheanh.local>
2017-03-03 17:15:54 +0700

    Initial Commit

---
+++ QLDA_IOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QLDA_IOS
+//
+//  Created by Hoang The Anh on 03/03/2017.
+//  Copyright © 2017 Harmony Soft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
