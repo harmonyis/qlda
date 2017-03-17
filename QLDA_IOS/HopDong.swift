a331cd82a8f5330fb864b7bc62eebd7e26a5c8c5
Hoang The Anh <theanhkazu@hoangtheanh.local>
2017-03-17 08:59:06 +0700

    TA-

---
+++ QLDA_IOS/HopDong.swift
@@ -0,0 +1,28 @@
+//
+//  HopDong.swift
+//  QLDA_IOS
+//
+//  Created by Hoang The Anh on 16/03/2017.
+//  Copyright © 2017 Harmony Soft. All rights reserved.
+//
+
+import Foundation
+class HopDong{
+init() {
+    TenGoiThau = ""
+    GiaTriHopDong = ""
+    DonViThucHien = ""
+    SoNK = ""
+    LoaiHD = ""
+    ThoiGianThucHien = ""
+    GiaTriLK = ""
+    
+}
+var TenGoiThau : String?
+var GiaTriHopDong : String?
+var DonViThucHien : String?
+var ThoiGianThucHien : String?
+var SoNK : String?
+var LoaiHD : String?
+var GiaTriLK : String?
+}
---
+++ QLDA_IOS/NhomHopDong.swift
@@ -0,0 +1,21 @@
+//
+//  NhomHopDong.swift
+//  QLDA_IOS
+//
+//  Created by Hoang The Anh on 16/03/2017.
+//  Copyright © 2017 Harmony Soft. All rights reserved.
+//
+
+import Foundation
+class NhomHopDong{
+    init() {
+        LoaiHopDong = ""
+        GiaTriHopDong = ""
+        GiaTriLK = ""
+        NhomHopDong = [HopDong]()
+    }
+    var LoaiHopDong : String?
+    var GiaTriHopDong : String?
+    var GiaTriLK : String?
+    var NhomHopDong : [HopDong]?
+}
