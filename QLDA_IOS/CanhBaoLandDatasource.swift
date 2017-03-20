52694786c8dea5957ff41a6698432311daad1f9c
dungnn <dung_n@harmonysoft.com.vn>
2017-03-20 14:03:35 +0700

    commit Cảnh báo land

--- QLDA_IOS/CanhBaoLandDatasource.swift
+++ QLDA_IOS/CanhBaoLandDatasource.swift
@@ -11,6 +11,8 @@ class CanhBaoLandDatasource : NSObject, UITableViewDelegate, UITableViewDataSour
     
     var arrCanhBao : [CanhBaoEntity]
     var table : UITableView
+    var setHeaderClick : Set<String> = []
+    let myColorBoder : UIColor = UIColor(netHex: 0xcccccc)
     
     init (arrData : [CanhBaoEntity],table : UITableView) {
         self.arrCanhBao = arrData
@@ -22,19 +24,56 @@ class CanhBaoLandDatasource : NSObject, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.arrCanhBao[section].arrSection.count
+        if self.setHeaderClick.contains("\(section)") {
+            return 0
+        } else {
+            return arrCanhBao[section].arrSection.count
+        }
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let type = arrCanhBao[indexPath.section].arrSection[indexPath.row].type
         
         if type == -1 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CanhBaoTableViewCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cellHDTitleLand", for: indexPath) as! CellHopDongTitleLandTableViewCell
+            
+            
             let item = arrCanhBao[indexPath.section].arrSection[indexPath.row] as! CTHHDSection
-            cell.lblTenDuAn.text = item.title
             
+            cell.lbl1R1.text = item.title
+            cell.lbl2R2.text = "Tên hợp đồng"
+            cell.lbl3R2.text = "Ngày bắt đầu"
+            cell.lbl4R2.text = "Thời gian thực hiện (ngày)"
+            cell.lbl5R2.text = "Ngày kết thúc"
+            cell.lbl6R2.text = "Số ngày chậm"
             
+             cell.viewR1.viewWithTag(1)!.layer.addBorder(edge: .right, color: myColorBoder, thickness: 0.5)
+            cell.view2R2.layer.borderWidth = 0.5
+            cell.view2R2.layer.borderColor = myColorBoder.cgColor
+            //cell.view2R2.layer.addBorder(edge: .right, color: myColorBoder, thickness: 0.5)
+            //cell.view2R2.layer.addBorder(edge: .left, color: myColorBoder, thickness: 0.5)
             
+            cell.view1R2.layer.borderWidth = 0.5
+            cell.view1R2.layer.borderColor = myColorBoder.cgColor
+            
+            cell.view2R2.layer.borderWidth = 0.5
+            cell.view2R2.layer.borderColor = myColorBoder.cgColor
+            
+            cell.view3R2.layer.borderWidth = 0.5
+            cell.view3R2.layer.borderColor = myColorBoder.cgColor
+            
+            cell.view4R2.layer.borderWidth = 0.5
+            cell.view4R2.layer.borderColor = myColorBoder.cgColor
+            
+            cell.view5R2.layer.borderWidth = 0.5
+            cell.view5R2.layer.borderColor = myColorBoder.cgColor
+            
+            cell.view6R2.layer.borderWidth = 0.5
+            cell.view6R2.layer.borderColor = myColorBoder.cgColor
+            
+
+            
+            cell.layer.borderColor = myColorBoder.cgColor
+            cell.layer.borderWidth = 0.5
             
             return cell
         } else if type == 0 {
@@ -48,49 +87,80 @@ class CanhBaoLandDatasource : NSObject, UITableViewDelegate, UITableViewDataSour
             cell.lbl5.text = item.ngayKT
             cell.lbl6.text = item.ngayCham
             //cell.lblSoNgayCham.isHidden = true
+            cell.layer.borderColor = myColorBoder.cgColor
+            cell.layer.borderWidth = 0.5
             
+            cell.view1.layer.borderWidth = 0.5
+            cell.view1.layer.borderColor = myColorBoder.cgColor
+            
+            cell.view2.layer.borderWidth = 0.5
+            cell.view2.layer.borderColor = myColorBoder.cgColor
+            
+            cell.view3.layer.borderWidth = 0.5
+            cell.view3.layer.borderColor = myColorBoder.cgColor
+            
+            cell.view4.layer.borderWidth = 0.5
+            cell.view4.layer.borderColor = myColorBoder.cgColor
+            
+            cell.view5.layer.borderWidth = 0.5
+            cell.view5.layer.borderColor = myColorBoder.cgColor
+            
+            cell.view6.layer.borderWidth = 0.5
+            cell.view6.layer.borderColor = myColorBoder.cgColor
             
             return cell
             
         }
         else if type == 1 || type == 2{
-
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cellChamQTLand") as! CellChamQTLandTableViewCell
             let item = arrCanhBao[indexPath.section].arrSection[indexPath.row] as! ChamNHSQT
-            //cell.lblTitle.text = "Chậm nộp hồ sơ quyết toán"
+            
+            cell.lbl1R1.text = "Chậm nộp hồ sơ quyết toán"
+            
+            cell.lbl2R3.text = "\(item.ngayKyBB)"
+            cell.lbl3R3.text = item.thoiGianQD
+            cell.lbl4R3.text = item.ngayDuKienTrinh
+            cell.lbl5R3.text = item.ngayCham
+            cell.lbl6R3.text = item.ghiChu
             
             
             
-            let cell = tableView.dequeueReusableCell(withIdentifier: "cellHDLand") as! CellHopDongLandTableViewCell
+            cell.lbl2R2.text = "Ngày ký biên bản bàn giao đưa vào sử dụng"
+            cell.lbl3R2.text = "Thời gian quy định (tháng)"
+            cell.lbl4R2.text = "Ngày dự kiến trình HSQT"
+            cell.lbl5R2.text = "Số ngày chậm"
+            cell.lbl6R2.text = "Ghi chú"
+            cell.layer.borderColor = myColorBoder.cgColor
+            cell.layer.borderWidth = 0.5
             
-            cell.lbl2.text = "\(item.ngayKyBB)"
-            cell.lbl3.text = item.thoiGianQD
-            cell.lbl4.text = item.ngayDuKienTrinh
-            cell.lbl5.text = item.ngayCham
-            cell.lbl6.text = item.ghiChu
+
             
             return cell
             
         } else {
-            
-            let cell = tableView.dequeueReusableCell(withIdentifier: "cellHDLand") as! CellHopDongLandTableViewCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cellChamQTLand") as! CellChamQTLandTableViewCell
             
             let item = arrCanhBao[indexPath.section].arrSection[indexPath.row] as! ChamPDHSQT
-            //cell.lblTitle.text = "Chậm phê duyệt quyết toán"
+            cell.lbl1R1.text = "Chậm phê duyệt quyết toán"
             
-            cell.lbl2.text = "\(item.ngayNhanDuHS)"
-            cell.lbl3.text = item.thoiGianQD
-            cell.lbl4.text = item.ngayDuKienPD
-            cell.lbl5.text = item.ngayCham
-            cell.lbl6.text = item.ghiChu
+            cell.lbl2R3.text = "\(item.ngayNhanDuHS)"
+            cell.lbl3R3.text = item.thoiGianQD
+            cell.lbl4R3.text = item.ngayDuKienPD
+            cell.lbl5R3.text = item.ngayCham
+            cell.lbl6R3.text = item.ghiChu
+            
+            cell.lbl2R2.text = "Ngày nhận đủ hồ sơ quyết toán"
+            cell.lbl3R2.text = "Thời gian quy định (tháng)"
+            cell.lbl4R2.text = "Ngày dự kiến phê duyệt HSQT"
+            cell.lbl5R2.text = "Số ngày chậm"
+            cell.lbl6R2.text = "Ghi chú"
+            cell.layer.borderColor = myColorBoder.cgColor
+            cell.layer.borderWidth = 0.5
             
             return cell
         }
         
-        /*
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cellHDLand") as! CellHopDongLandTableViewCell
-        cell.lbl2.text = "đâhsi áidaisu hdiashdi áhdiashdiashdihs díhai dhaishdia hsdi áhduia hsiduahs idah"
-        return cell
- */
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
@@ -98,7 +168,34 @@ class CanhBaoLandDatasource : NSObject, UITableViewDelegate, UITableViewDataSour
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellHeader") as! CBHeaderTableViewCell
         cell.lblHeader.text = "\(arrCanhBao[section].titleSection)"
         
+        if self.setHeaderClick.contains("\(section)") {
+            cell.imgHeaderIcon.image = UIImage(named: "arrow_down")
+        }
+        
+        
+        cell.imgIcon.layer.borderColor = myColorBoder.cgColor
+        cell.imgIcon.layer.borderWidth = 0.5
+        cell.layer.borderColor = myColorBoder.cgColor
+        cell.layer.borderWidth = 0.5
+        
+        let eventClick : UITapGestureRecognizer = UITapGestureRecognizer()
+        cell.accessibilityLabel = "\(section)"
+        eventClick.addTarget(self, action:  #selector(CanhBaoDatasource.headerClick(sender: )))
+        
+        cell.addGestureRecognizer(eventClick)
+        cell.isUserInteractionEnabled = true;
+        
         return cell
         
+    }
+    
+    func headerClick(sender:UITapGestureRecognizer) {
+        let value = (sender.view?.accessibilityLabel!)! as String
+        if self.setHeaderClick.contains(value) {
+            self.setHeaderClick.remove(value)
+        } else {
+            self.setHeaderClick.insert(value)
+        }
+        self.table.reloadData()
     }
 }
--- QLDA_IOS/UIView.swift
+++ QLDA_IOS/UIView.swift
@@ -19,6 +19,8 @@ extension UIView{
         
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
     }
+    
+    
     /*
     func setBadge(tag : Int, number : Int, frame : CGRect){
         var containBadge = false
@@ -63,4 +65,32 @@ extension UIView{
         }
     }
  */
+}
+extension CALayer {
+    
+    func addBorder(edge: UIRectEdge, color: UIColor, thickness: CGFloat) {
+        
+        let border = CALayer()
+        
+        switch edge {
+        case UIRectEdge.top:
+            border.frame = CGRect.init(x: 0, y: 0, width: frame.width, height: thickness)
+            break
+        case UIRectEdge.bottom:
+            border.frame = CGRect.init(x: 0, y: frame.height - thickness, width: frame.width, height: thickness)
+            break
+        case UIRectEdge.left:
+            border.frame = CGRect.init(x: 0, y: 0, width: thickness, height: frame.height)
+            break
+        case UIRectEdge.right:
+            border.frame = CGRect.init(x: frame.width - thickness, y: 0, width: thickness, height: frame.height)
+            break
+        default:
+            break
+        }
+        
+        border.backgroundColor = color.cgColor;
+        
+        self.addSublayer(border)
+    }
 }
