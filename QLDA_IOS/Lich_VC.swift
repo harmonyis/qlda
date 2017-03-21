db6ef64d5763f3f569a3f306e1cc413175f79e9e
datlh <ledat010994@gmail.com>
2017-03-21 17:10:18 +0700

    update lịch

--- QLDA_IOS/Lich_VC.swift
+++ QLDA_IOS/Lich_VC.swift
@@ -11,12 +11,19 @@ import FSCalendar
 
 class Lich_VC: Base_VC, FSCalendarDelegate, FSCalendarDataSource {
 
+    @IBOutlet weak var fsCalendar: FSCalendar!
+    var listEventCurrentMonth : [Int] = [Int]()
+    var listEventPreMonth : [Int] = [Int]()
+    var listEventNextMonth : [Int] = [Int]()
+    
+    let currentDate = Date()
+    let calendar = Calendar.current
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        getCalendarByUserAndDate(currentDate)
         // Do any additional setup after loading the view.
     }
-    
-    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -31,52 +38,96 @@ class Lich_VC: Base_VC, FSCalendarDelegate, FSCalendarDataSource {
         }
     }
     
-    func calendar(_ calendar: FSCalendar, willDisplay cell: FSCalendarCell, for date: Date, at monthPosition: FSCalendarMonthPosition) {
-        print(date)
-        DispatchQueue.global(qos: .userInitiated).async {
-            DispatchQueue.main.async {
-                let img : UIImageView = UIImageView()
-                img.image = #imageLiteral(resourceName: "ic_online")
-                //img.frame = CGRect(x: cell.frame.width/2 - 3, y: cell.frame.height - 15 , width: 6, height: 6)
-                img.translatesAutoresizingMaskIntoConstraints = false
-                cell.addSubview(img)
-                
-                let horizontalConstraint = NSLayoutConstraint(item: img, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: cell, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0)
-                let verticalConstraint = NSLayoutConstraint(item: img, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: cell, attribute: NSLayoutAttribute.centerY, multiplier: 1, constant: 8)
-                let widthConstraint = NSLayoutConstraint(item: img, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 5)
-                let heightConstraint = NSLayoutConstraint(item: img, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 5)
-                cell.addConstraints([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
-            }
+    func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
+        let day = self.calendar.component(.day, from: date)
+        let filler = self.listEventCurrentMonth.filter(){$0 == day}
+        if filler.count != 0{
+            return 1
         }
-        
+        return 0
+    }
+    func calendarCurrentPageDidChange(_ calendar: FSCalendar) {
+        let month =  self.calendar.component(.month, from: calendar.currentPage)
+        print("\(month)")
     }
     
-    
-    
     func getCalendarByUserAndDate(_ date : Date){
+        let month = self.calendar.component(.month, from: date)
+        let year = self.calendar.component(.year, from: date)
+        let apiUrl : String = "\(UrlPreFix.Map.rawValue)/getEventOfMonth"
         
-        let apiUrl : String = "\(UrlPreFix.Map.rawValue)/getCalendarByUserAndDate"
-        
-        let params : String = "{\"nUserID\" : \(Config.userID), \"dtDateView\": \(date)}"
-        ApiService.Post(url: apiUrl, params: params, callback: callbackCreateGroup, errorCallBack: { (error) in
+        let params : String = "{\"nUserID\" : \(Config.userID), \"nMonth\": \(month), \"nYear\":\(year)}"
+        print(params)
+        ApiService.Post(url: apiUrl, params: params, callback: { (data) in
+            let json = try? JSONSerialization.jsonObject(with: data, options: [])
+            if let methodResult = json as? [String:Any] {
+                if let items = methodResult["getEventOfMonthResult"] as? [String] {
+                    for item in items{
+                        let time = Date(jsonDate: item)
+                        let day = self.calendar.component(.day, from: time!)
+                        self.listEventCurrentMonth.append(day)
+                        self.fsCalendar.reloadData()
+                        self.reloadCalendar()
+                    }
+                }
+            }
+            
+        }, errorCallBack: { (error) in
             print("error")
             print(error.localizedDescription)
         })
     }
     
-    func callbackCreateGroup(data : Data) {
-        let json = try? JSONSerialization.jsonObject(with: data, options: [])
-        if let dic = json as? [String:Any] {
-            //if let groupID = dic["Chat_CreateGroupChatResult"] as? Int {
-            if (dic["Chat_CreateGroupChatResult"] as? Int) != nil {
-                DispatchQueue.main.async(execute: { () -> Void in
-                   
-                })
-                
+    func reloadCalendar(){
+        DispatchQueue.global(qos: .userInitiated).async {
+            DispatchQueue.main.async {
+                self.fsCalendar.reloadData()
             }
         }
-        
     }
-    
 }
+
+class CalendarYear{
+    init() {
+        months = [CalendarMonth]()
+    }
+    var year : Int?
+    var months : [CalendarMonth]
+}
+class CalendarMonth{
+    init() {
+        days = [CalendarDay]()
+    }
+    var month : Int?
+    var days : [CalendarDay]
+}
+class CalendarDay{
+    init() {
+        items = [CalendarItem]()
+    }
+    var day : Int?
+    var items : [CalendarItem]?
+}
+class CalendarItem{
+    var calendarScheduleID : Int?
+    
+    var title : String?
+    
+    var note : String?
+    
+    var UserID : Int?
+    
+    var dateSchedule : Date?
+    
+    var timeStart : Date?
+    
+    var timeEnd : Date?
+}
+
+
+
+
+
+
+
 
